ed91469994c24c7afc6ffd2ba8cbc95e7c34a220
fkaragh <fkfurkankara@gmail.com>
2024-09-25 13:00:39 +0300

    Components added

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+
+  constructor(private investmentService: InvestmentService){}
+
+  onSubmit(){
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration : +this.duration
+    });
+  }
+
+}
